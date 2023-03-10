b1a89fdd1c8b0e99769cee6f5c5d32f0bf3c0bb1
Divya <36568016+divdiv8@users.noreply.github.com>
2023-03-09 21:42:50 -0500

    derange IP

---
+++ leetcode/DerangeIP.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/defanging-an-ip-address/description/
+class Solution {
+    public String defangIPaddr(String address) {
+        String[] strSplit = address.split("");
+  
+        // Now convert string into ArrayList
+        ArrayList<String> strList = new ArrayList<String>(
+            Arrays.asList(strSplit));
+        String strList2 ="";
+        //System.out.println(strList2+"strList2");
+        // Now print the ArrayList
+        for (int i = 0; i < strList.size(); i++)
+        {
+            if(strList.get(i).charAt(0)=='.')
+                strList2+="[.]";
+            else
+                strList2+=strList.get(i).charAt(0);
+
+        }
+        return strList2;
+    }
+}
